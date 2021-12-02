2fee5aea11484809fff77dd46944366605efc6db
Arturo Quiroz <r2inlondon@gmail.com>
2021-12-02 22:20:22 +0000

    still working on the form

--- src/components/AddCocktailPage.js
+++ src/components/AddCocktailPage.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
 import CocktailForm from "./CocktailForm";
 
-const AddCocktailPage = () => {
-  const dispatch = useDispatch();
-
+const AddCocktailPage = (props) => {
+  
   return (
     <div>
       <h1>Add a cocktail</h1>
-      <CocktailForm />
+      <CocktailForm onSubmit={(props) => {
+        console.log(props)
+      }}/>
     </div>
   );
 };
--- src/components/CocktailForm.js
+++ src/components/CocktailForm.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 
-const CocktailForm = () => {
+const CocktailForm = (props) => {
   const [name, setCocktail] = useState("");
 
   const onSubmit = (e) => {
-    console.log("clicked", name);
     e.preventDefault();
   };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
