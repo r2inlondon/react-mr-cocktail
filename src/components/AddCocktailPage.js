ec8464e43a56f1d16d98ec243fd386640e148cda
Arturo Quiroz <r2inlondon@gmail.com>
2021-12-07 21:24:38 +0000

    AddCocktail is now navigating to ShowCocktail

--- src/components/AddCocktailPage.js
+++ src/components/AddCocktailPage.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
-import { useNavigate } from "react-router";
+import { useParams, useNavigate } from "react-router";
 import { createCocktail } from "../Reducers/cocktailsReducer";
 import CocktailForm from "./CocktailForm";
 
 const AddCocktailPage = ({ dispatch }) => {
   let navigate = useNavigate();
+
   return (
     <div>
       <h1>Add a cocktail</h1>
       <CocktailForm
         onSubmit={(cocktail) => {
           dispatch(createCocktail(cocktail));
-          navigate("/");
+          navigate(`/edit/${cocktail.id}`);
         }}
       />
     </div>
