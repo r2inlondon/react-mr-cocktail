fa1b537b4dc952fcfa25b2dd7eee90d0101225a3
Arturo Quiroz <r2inlondon@gmail.com>
2022-01-02 19:34:35 +0000

    replaced connect for useSelector on the AddCocktailPage

--- src/components/AddCocktailPage.js
+++ src/components/AddCocktailPage.js
@@ -1,19 +1,26 @@
 import React from "react";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 import { startCreateCocktail } from "../Reducers/cocktailsReducer";
 import CocktailForm from "./CocktailForm";
 
-const AddCocktailPage = ({ dispatch }) => {
+const AddCocktailPage = () => {
+  const dispatch = useDispatch();
+  let navigate = useNavigate();
+  const cocktailsState = useSelector((state) => state.cocktails);
+
   return (
     <div>
       <h1>Add a cocktail</h1>
       <CocktailForm
         onSubmit={(cocktail) => {
           dispatch(startCreateCocktail(cocktail));
+          console.log(cocktailsState);
+          // navigate(`/edit/${cocktail.id}`);
         }}
       />
     </div>
   );
 };
 
-export default connect()(AddCocktailPage);
+export default AddCocktailPage;
