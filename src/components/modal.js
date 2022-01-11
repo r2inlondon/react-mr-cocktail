a17b340b172b2604e3aebae039215de644407e90
Arturo Quiroz <r2inlondon@gmail.com>
2022-01-11 22:14:35 +0000

    SASS and useEffected added for the progressBar compoment

--- src/components/modal.js
+++ src/components/modal.js
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 const Modal = ({ openModal, children, closeModal }) => {
   const cocktailsState = useSelector((state) => state.cocktails);
 
+  // Get the last cocktail from the state
   let myCocktail = cocktailsState.slice(-1);
 
   if (!openModal) return null;
