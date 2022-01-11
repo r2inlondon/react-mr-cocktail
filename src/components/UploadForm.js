1914be42c6097a1ee3398f048f293d812e8231d1
Arturo Quiroz <r2inlondon@gmail.com>
2022-01-11 21:39:10 +0000

    ProgressBar and useStorage Hook created

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import ProgressBar from "./ProgressBar";
 
 const UploadForm = () => {
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+
+  const types = ["image/png", "image/jpeg"];
+
   const changeHandler = (e) => {
     let selected = e.target.files[0];
-    console.log(selected);
+
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+      setError(null);
+    } else {
+      setFile(null);
+      setError("Select an image file (png or jpeg)");
+    }
   };
 
   return (
     <form>
       <input type="file" onChange={changeHandler}></input>
+      <div className="output">
+        {error && <div className="error">{error}</div>}
+        {file && <div>{file.name}</div>}
+        {file && <ProgressBar file={file} setFile={setFile} />}
+      </div>
     </form>
   );
 };
---
+++ src/hooks/useStorage.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { projectStorage } from "../firebase/firebase";
+
+const useStorage = (file) => {
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(null);
+  const [url, setUrl] = useState(null);
+
+  useEffect(() => {
+    const storageRef = projectStorage.ref(`images/${file.name}`);
+
+    storageRef.put(file).on(
+      "state_change",
+      (snapshot) => {
+        let percentage =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setProgress(percentage);
+      },
+      (err) => {
+        setError(err);
+      },
+      async () => {
+        const url = await storageRef.getDownloadURL();
+        setUrl(url);
+      }
+    );
+  }, [file]);
+  console.log(url);
+  return { progress, error, url };
+};
+
+export default useStorage;
