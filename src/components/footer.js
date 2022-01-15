ecff41ad9d858761aa7cba7372877437a5e67c5e
Arturo Quiroz <r2inlondon@gmail.com>
2022-01-15 17:42:42 +0000

    Footer component has been set up with minimun style

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,19 @@ const Footer = () => (
   <div className="footer">
     <div className="social">
       <a href="#0">Instagram</a>
+      <a href="#0">facebook</a>
+      <a href="#0">youtube</a>
+    </div>
+    <div className="blog">
+      <div className="blog-text">
+        <h2>Blog</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur vero
+          corporis ipsum repellendus cumque, laboriosam consectetur maxime
+          temporibus amet minus modi possimus cum unde fugit est. Dolores autem
+          eius voluptate.
+        </p>
+      </div>
     </div>
   </div>
 );
