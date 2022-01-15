12c09ee0b40e4014bd5923814e85679c707f1723
Arturo Quiroz <r2inlondon@gmail.com>
2022-01-15 15:40:59 +0000

    cocktails photos and names have been added to the home page as link

--- src/components/ListCocktailItem.js
+++ src/components/ListCocktailItem.js
@@ -3,15 +3,21 @@ import { Link } from "react-router-dom";
 import black from "../styles/images/black.png";
 
 const ListCocktialItem = ({ id, name, defaultImage }) => {
-  // if (defaultImage === 0) {
-  //   defaultImage = black;
-  // }
+  if (defaultImage === 0) {
+    defaultImage = black;
+  }
 
   return (
     <div>
-      <h3>
-        <Link to={`/edit/${id}`}>{name}</Link>
-      </h3>
+      <Link
+        to={`/edit/${id}`}
+        className="cocktail-image"
+        style={{
+          backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${defaultImage})`,
+        }}
+      >
+        <h1 className="cocktail-name">{name}</h1>
+      </Link>
     </div>
   );
 };
