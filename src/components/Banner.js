b9c464da73a117a83aaeb7f8d9114fcab2ccede9
Arturo Quiroz <r2inlondon@gmail.com>
2022-01-16 11:26:05 +0000

    Banner - media queries rules added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,7 @@ import React from "react";
 const Banner = () => (
   <div className="banner">
     <div className="banner-container">
-      <h1>Crud Web App built with React, Redux and Firebase</h1>
+      <h1>Mr Cocktail web app</h1>
       <p>
         Check out our cocktails recipies and why not add your favorite cocktail
         !
