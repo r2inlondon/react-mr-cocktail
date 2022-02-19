87f6503442917fc6bc5d3c797fb7354396aa2a58
Arturo Quiroz <r2inlondon@gmail.com>
2022-02-19 21:23:32 +0000

    prefixed.js with the json data created

---
+++ src/firebase/prefixed.js
@@ -0,0 +1,35 @@
+{
+    "cocktails" : {
+      "-MtFV3rL2ykreuZEed2V" : {
+        "defaultImage" : "https://firebasestorage.googleapis.com/v0/b/mr-cocktail-a9ab4.appspot.com/o/images%2FMargarita.jpg?alt=media&token=00869c79-cdc6-4761-844c-132769b31792",
+        "ingredients" : [ 0, "50ml tequila reposado", null, "20ml triple sec (we used Cointreau)" ],
+        "name" : "Margarita"
+      },
+      "-MtFVvuc_d5BBuBnZhry" : {
+        "defaultImage" : "https://firebasestorage.googleapis.com/v0/b/mr-cocktail-a9ab4.appspot.com/o/images%2Fred-dog-saloon-michelada-sun-09412-0621.jpg?alt=media&token=17fb5ef0-c5a6-4a23-bf91-b90b24a1eb6b",
+        "ingredients" : [ 0, "1 tsp salt", "1 tsp chilli powder", "1 tsp black pepper", "ice", "3 shakes hot sauce", "Mexican lager" ],
+        "name" : "Michelada"
+      },
+      "-MtTdJVa_hUtVHBKxv9X" : {
+        "defaultImage" : "https://firebasestorage.googleapis.com/v0/b/mr-cocktail-a9ab4.appspot.com/o/images%2Fmai-tai.jpg?alt=media&token=01e26093-6b3f-42ca-b19a-db6a0f393d2c",
+        "ingredients" : [ 0, "ice" ],
+        "name" : "Mai Thai"
+      },
+      "-MtXR_ASl5WwZDoJE-GY" : {
+        "defaultImage" : "https://firebasestorage.googleapis.com/v0/b/mr-cocktail-a9ab4.appspot.com/o/images%2F2022-01-16_10h31_42.png?alt=media&token=a7b5898a-f293-461a-9d7c-15bd6e50104a",
+        "ingredients" : [ 0 ],
+        "name" : "Porn Star Martini"
+      },
+      "-MuHqSXjCbvb3ic8pdrg" : {
+        "defaultImage" : "https://firebasestorage.googleapis.com/v0/b/mr-cocktail-a9ab4.appspot.com/o/images%2Fhow-to-make-the-perfect-old-fashioned-cocktail-_-man-of-many.png?alt=media&token=4760e9c3-7ddf-498d-802b-28fc98f4d3fe",
+        "ingredients" : [ 0, "2 tsp sugar syrup or 1 tsp granulated sugar", "1-2 dashes Angostura bitters", "60ml Scotch whisky or bourbon", "orange slice" ],
+        "name" : "Old Fashioned"
+      },
+      "-MuSQekRpurZNqeDd3W8" : {
+        "defaultImage" : "https://firebasestorage.googleapis.com/v0/b/mr-cocktail-a9ab4.appspot.com/o/images%2Fb52-shot-735x1103.jpg?alt=media&token=bc6b2be7-af9e-4d5c-966b-9af7d610f5f8",
+        "ingredients" : [ 0, "1/3 ounce coffee liqueur ", "1/3 ounce Baileys Irish cream liqueur ", "1/3 ounce Grand Marnier liqueur " ],
+        "name" : "B52"
+      }
+    }
+  }
+  
